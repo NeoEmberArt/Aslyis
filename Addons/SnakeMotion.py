d86ecec995f09edee34df747166d468a608188ee
NeoEmberArts <authoranimation@gmail.com>
2022-04-16 19:40:42 -0400

    Update SnakeMotion.py

--- Addons/SnakeMotion.py
+++ Addons/SnakeMotion.py
@@ -273,6 +273,10 @@ class SNAKE_OT_Create(Operator):
         return (bpy.context.mode == "OBJECT") and wassetup
 
     def execute(self, context):
+	global nodes
+	global objects
+	nodes = []
+	objects = []
         #get settings
         keyframe_frequency_setting = context.scene.snakeAnim.keyframe_frequency_setting
         frame_subdivisions = context.scene.snakeAnim.frame_subdivisions
